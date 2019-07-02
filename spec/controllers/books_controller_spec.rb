9ac54a1a9afbe5019ab1329464a2dd3daf427bc4
Mario Olivio Flores <rio517@gmail.com>
2019-07-02 14:42:23 +0200

    Add additional failure case
    This case actually results in a type error. It seems that in controller
    specs, rspec expects a hash for params.

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -13,5 +13,15 @@ RSpec.describe BooksController, type: :controller do
         expect(response.body).to eq(sample_body.to_json.to_s)
       end
     end
+
+    # Results in: undefined method `symbolize_keys' for "{\"data\":{\"title\":\"book title\",\"author\":\"John Doe\"}}":String
+    context 'with parms.to_json' do
+      it 'receives a body' do
+        sample_body = {data: {title: 'book title', author: 'John Doe'}}
+        post :create, params: sample_body.to_json
+        expect(response).to have_http_status(:success)
+        expect(response.body).to eq(sample_body.to_json.to_s)
+      end
+    end
   end
 end
